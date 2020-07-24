b1799a518347db96b8467186139e66e67f8ef305
senadakadric <skadric@fordham.edu>
2020-07-24 14:43:35 -0400

    fixed cors, added to application controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationsController < ApplicationController
     # THIS IS OUR APPLICATIONS CONTROLLER
+    def index
+        applications = Application.all
+
+        render json: applications
+    end
 end
