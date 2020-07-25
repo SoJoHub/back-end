d8055529be49f63176aa4a7625cfb343ccdaab01
ccedacero <cristian.cedacero@gmail.com>
2020-07-25 18:55:00 -0400

    auth backend

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :applications
   resources :job_listings
   resources :users , only: [:create]
-  post "/login", to: "user#login"
+  post "/login", to: "users#login"
   get "/auto_login", to: "users#auto_login"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
