b66f492258d9507965f978476879ab51b830db3f
ccedacero <cristian.cedacero@gmail.com>
2020-07-26 17:46:23 -0400

    add comment routes

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,3 +1,18 @@
 class TopicsController < ApplicationController
 
+    def index 
+        @topics = Topic.all 
+        render json: @topics
+    end
+
+    def show 
+        # byebug
+        @topic = Topic.find(params[:id])
+        @comments = @topic.comments
+        render json: {
+            topic: @topic,
+            comments: @comments
+        }
+    end
+
 end
