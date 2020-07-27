80fb0e195b09ee867591f87cf2d9aac6f84b24b7
hkofkin <hkofkin@gmail.com>
2020-07-27 15:12:29 -0500

    accessing application data correctly

--- app/serializers/application_serializer.rb
+++ app/serializers/application_serializer.rb
@@ -1,3 +1,3 @@
 class ApplicationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :job_listing_id, :status, :created_at
+  attributes :id, :user_id, :job_listing_id, :status, :created_at, :user
 end
