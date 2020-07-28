deb986a0003c5b131208bd23f46208a2e4507842
senadakadric <skadric@fordham.edu>
2020-07-28 17:40:55 -0400

    interview serializer and controller updated for deletion

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,6 +1,26 @@
 class InterviewsController < ApplicationController
+    # before_action :authorized
     def index
         interviews = @user.interviews
         render json: interviews
+    end
+
+    def show
+        interview = Interview.find(params[:id])
+
+         render json: interview
+    end
+
+    def destroy 
+        interview = Interview.find(params["id"])
+        deleted = interview.destroy
+
+        render json: deleted
+    end
+
+    private
+
+    def interview_params
+        params.permit(:id)
     end
 end
--- app/serializers/interview_serializer.rb
+++ app/serializers/interview_serializer.rb
@@ -1,4 +1,6 @@
 class InterviewSerializer < ActiveModel::Serializer
+  belongs_to :user
+  belongs_to :application
   attributes :id, :application_id, :date, :complete, :interviewer, :location, :notes, :created_at, :user_id, :application_company
 
   def application_company
