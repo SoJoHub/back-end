1070c8e554eeb603a12fdd60c6f8fab4eb76afd2
ccedacero <cristian.cedacero@gmail.com>
2020-07-28 23:03:58 -0400

    back-end comment routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,18 +8,31 @@ class CommentsController < ApplicationController
     #   render json: @comments
     # end
 
+    def update
+    comment =Comment.find(comments_params[:id])
+    comment.update(content:params["content"])
+    render json: comment
+    end
+
     def create 
         # byebug 
-        comment = Comment.create(user_id: @user.id, topic_id: comments_params["topic_id"], content: comments_params["comment"])
+        comment = Comment.create(user_id: @user.id, topic_id: comments_params["topic_id"], content: comments_params["content"])
         render json: {comment: comment, user_name: @user.name}
     end 
+   
+
+    def destroy 
+      comment = Comment.find(comments_params["id"])
+      deletedComment = comment.destroy
+      render json: deletedComment
+  end
 
 
 
     private
   
     def comments_params
-      params.permit(:comment, :topic_id)
+      params.permit(:comment,:content, :topic_id, :id)
     end
 
 end
