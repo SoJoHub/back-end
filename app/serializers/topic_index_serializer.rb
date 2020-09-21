87b1e4d15da6bf7148167423967e1af2930c1139
bashir2017 <bashiralhanshali@yahoo.com>
2020-09-21 11:48:56 -0400

    update

--- app/serializers/topic_index_serializer.rb
+++ app/serializers/topic_index_serializer.rb
@@ -1,5 +1,5 @@
 class TopicIndexSerializer < ActiveModel::Serializer
-    attributes :id, :forum_id, :user_id, :title, :created_at :user_name
+    attributes :id, :forum_id, :user_id, :title, :created_at, :user_name
     
     def user_name 
         self.object.user.name 
